642a9316f37404a373e2b6cafa9cd410fc01629e
Marin Balabanov <marincomics@gmail.com>
2022-08-31 22:20:35 +0200

    Corrects styles and animations

--- counter/counter.js
+++ counter/counter.js
@@ -1,19 +1,22 @@
-const counterArea = document.getElementById('counterArea')
+const COUNTER_AREA = document.getElementById('counterArea')
 let abort = false;
 
 function setupApp() {
     abort = true;
-    counterArea.innerText = ''
+    COUNTER_AREA.innerText = ''
     document.getElementById('submitButton').removeAttribute('disabled')
+    document.getElementById('countDownSeconds').removeAttribute('disabled')
     document.getElementById('countDownSeconds').value = '10'
     document.getElementById('changeSettingsDuringCountdown').setAttribute('class', 'transparent-input hideElement')
     document.getElementById('resetButton').setAttribute('class', 'styledButton hideElement')
     document.getElementById('rocket-ship').classList.remove('liftoff')
-    document.getElementById('smoke').classList.remove('smoke-launch')
+    document.getElementById('smoke-left').classList.remove('smoke-launch')
+    document.getElementById('smoke-right').classList.remove('smoke-launch')
 
     document.getElementById('submitButton').addEventListener('click', function (event) {
         event.preventDefault()
         document.getElementById('submitButton').setAttribute('disabled', '')
+        document.getElementById('countDownSeconds').setAttribute('disabled', '')
         document.getElementById('changeSettingsDuringCountdown').classList.remove('hideElement')
         document.getElementById('resetButton').classList.remove('hideElement')
         document.getElementById('resetButton').addEventListener('click', setupApp)
@@ -23,8 +26,6 @@ function setupApp() {
         let countDownSeconds = document.getElementById('countDownSeconds')
         runCountdown(countDownSeconds.value)
     })
-
-    console.log('Setup complete.')
 }
 
 function runCountdown(totalSeconds) {
@@ -39,19 +40,20 @@ function runCountdown(totalSeconds) {
             clearInterval(SECONDS_COUNTER)
             totalSeconds = 'Liftoff!'
             document.getElementById('rocket-ship').setAttribute('class', 'liftoff')
-            document.getElementById('smoke').setAttribute('class', 'smoke-launch')
+            document.getElementById('smoke-left').setAttribute('class', 'smoke-launch')
+            document.getElementById('smoke-right').setAttribute('class', 'smoke-launch')
         }
-        counterArea.innerText = totalSeconds
+        COUNTER_AREA.innerText = totalSeconds
         totalSeconds--
     }, 1000)
 }
 
 function increaseSeconds() {
-    let increasedValue = counterArea.innerText
+    let increasedValue = COUNTER_AREA.innerText
     if (!isNaN(increasedValue)) {
         increasedValue = Number(increasedValue) + 1
         abort = true
-        counterArea.innerText = increasedValue
+        COUNTER_AREA.innerText = increasedValue
         document.getElementById('increaseSeconds').setAttribute('disabled', '')
         document.getElementById('decreaseSeconds').setAttribute('disabled', '')
         setTimeout(function () {
@@ -65,11 +67,11 @@ function increaseSeconds() {
 }
 
 function decreaseSeconds() {
-    let decreasedValue = counterArea.innerText
+    let decreasedValue = COUNTER_AREA.innerText
     if (!isNaN(decreasedValue)) {
         decreasedValue = Number(decreasedValue) - 1
         abort = true
-        counterArea.innerText = decreasedValue
+        COUNTER_AREA.innerText = decreasedValue
         document.getElementById('increaseSeconds').setAttribute('disabled', '')
         document.getElementById('decreaseSeconds').setAttribute('disabled', '')
         setTimeout(function () {
